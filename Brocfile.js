488a0a68000e30824270f7fe4299522ee80be339
Alinaki <mr.alinaki@gmail.com>
2017-08-08 02:09:23 +0300

    some cleanup

--- Brocfile.js
+++ Brocfile.js
@@ -13,23 +13,24 @@ var port = +process.argv.join('').split(/--port=?/)[1] || 4200;
 
 var Funnel = require('broccoli-funnel'); // Filter trees
 var MergeTrees = require('broccoli-merge-trees'); // Merge trees
+var Sass = require('broccoli-sass'); // SASS compiler
+var Sync = require('broccoli-browser-sync-bv'); // BrowserSync
+var AssetRev = require('broccoli-asset-rev'); // Assets Revision
 var concat = require('broccoli-concat'); // Concatenate trees
 var babel = require('broccoli-babel-transpiler'); // Babel transpiler
 var uglify = require('broccoli-uglify-sourcemap'); // UglifyJS
-var Sass = require('broccoli-sass'); // SASS compiler
-var watchify = require('broccoli-watchify');
+var watchify = require('broccoli-watchify'); // Watchify
 var autoprefixer = require('broccoli-autoprefixer'); // Autoprefixer
-var Sync = require('broccoli-browser-sync-bv'); // BrowserSync
-var AssetRev = require('broccoli-asset-rev'); // Assets Revision
 
 var srcDir = 'src';
 var sassDir = srcDir + '/scss';
 var jsDir = srcDir + '/js';
 
 // Transpile the source files
-var appJs = babel(jsDir);
+var scripts = babel(jsDir);
 
-appJs = watchify(appJs, {
+// And process them through browserify
+scripts = watchify(scripts, {
   browserify: {
     entries: ['index.js'],
     paths: ['.', __dirname + '/node_modules'],
@@ -38,7 +39,7 @@ appJs = watchify(appJs, {
 });
 
 if (BUILDING) {
-  appJs = uglify(appJs, {
+  scripts = uglify(scripts, {
     mangle: {
       except: ['global']
     }
@@ -51,11 +52,11 @@ if (BUILDING) {
 // ];
 
 // if (legacyFiles.length) {
-//   var vendorJs = new Funnel('node_modules', {
+//   var legacyJs = new Funnel('node_modules', {
 //     files: legacyFiles
 //   });
 
-//   legacyJs = concat(vendorJs, {
+//   legacyJs = concat(legacyJs, {
 //     // inputFiles: ['**/*.js'],
 //     headerFiles: legacyFiles,
 //     outputFile: 'static/js/legacy.js'
@@ -76,7 +77,7 @@ styles = autoprefixer(styles, {
 
 // Grab the HTML
 var html = new Funnel(srcDir, {
-  // files: ['index.html']
+  // files: ['index.html'],
   include: ['*.html', 'favicon.ico']
 });
 
@@ -103,24 +104,24 @@ var fonts = new Funnel(srcDir, {
 
 var treesToMerge = [
   html,
-  appJs,
+  scripts,
   styles,
   images,
   share,
   fonts,
 ];
 
-var exports = new MergeTrees(treesToMerge);
+var output = new MergeTrees(treesToMerge);
 
 if (BUILDING) {
-  exports = new AssetRev(exports, {
+  output = new AssetRev(output, {
     extensions: ['js', 'css', 'png', 'jpg', 'gif'],
     replaceExtensions: ['html', 'js', 'css'],
-    exclude: ['share/fb.jpg', 'share/vk.jpg', 'js/check.js'],
+    exclude: ['share/fb.jpg', 'share/vk.jpg'],
   });
 } else if (SERVING) {
   // Set up live reloading via BrowserSync
-  var browserSync = new Sync([html, appJs, styles, images, share, fonts], {
+  var browserSync = new Sync([html, scripts, styles, images, share, fonts], {
     port: port,
     browserSync: {
       port: 9000,
@@ -130,12 +131,12 @@ if (BUILDING) {
     }
   });
 
-  exports = new MergeTrees([
-    exports,
+  output = new MergeTrees([
+    output,
     browserSync
   ]);
 }
 
 // Grab all our trees and
 // export them as a single and final tree
-module.exports = exports;
+module.exports = output;
